0772baa2ecd06c6f83ed8d1657ad01c7cc75501f
Erij Osman <erij7th@gmail.com>
2025-02-20 12:38:18 -0500

    Todo app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'todo',
     'viewsbasics.apps.ViewsbasicsConfig',
     'dtl.apps.DtlConfig',
     'django.contrib.admin',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo',
 ]
 
 MIDDLEWARE = [
